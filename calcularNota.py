2833c67b38cc19ca0566106fbf11f4d7958742d7
Alex <beccialex2002@gmail.com>
2023-11-04 06:44:36 +0100

    Prom notes

---
+++ calcularNota.py
@@ -0,0 +1,9 @@
+#Se necesita obtener el promedio simple de un estudiante a partir de sus tres notas parciales N1, N2 y N3.
+
+n1 = float(input("Ingrese nota n1: "))
+n2 = float(input("Ingrese nota n2: "))
+n3 = float(input("Ingrese nota n3: "))
+
+prom = (n1+n2+n3)/3
+
+print("Promedio: ",prom)
