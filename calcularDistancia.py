557463d4df5e59f850772f345b5f7ea525b24d83
Alex <beccialex2002@gmail.com>
2023-11-04 06:44:31 +0100

    distance

---
+++ calcularDistancia.py
@@ -0,0 +1,10 @@
+#Se desea calcular la distancia recorrida (m) por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que es un MRU (Movimiento Rectilíneo Uniforme).
+
+
+print("Ingrese la velocidad y el tiempo de la unidad: ")
+velocity= float(input("Velocidad: "))
+time = float(input("Tiempo: "))
+
+distance = velocity*time
+200
+print("Velocidad Recorrida: ",distance)
